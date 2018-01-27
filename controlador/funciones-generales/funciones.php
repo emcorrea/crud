3ba5acffe2aeda5a90f86a247b86ea8fc9980227
Emilio Correa <em.correa.soto@gmail.com>
2018-01-26 23:33:56 -0300

    Actualizar

---
+++ controlador/funciones-generales/funciones.php
@@ -0,0 +1,24 @@
+<?php
+/*CRUD - CREATE,READ,UPDATE,DELETE*/
+
+//CREATE
+function grabar(){
+
+}
+//READ
+function leer(){
+
+}
+
+//UPDATE
+function actualizar(){
+
+}
+
+//DELETE
+function eliminar(){
+	
+}
+
+
+?>
---
+++ modelo/ModeloBD.sql
@@ -0,0 +1,21 @@
+CREATE TABLE ALUMNO(
+	rut INT(11),
+	nombre VARCHAR(50),
+	apellidoPaterno VARCHAR(50),
+	apellidoMaterno VARCHAR(50),
+	sexo CHAR(1),
+	fechaNacimiento DATE,
+	domicilio VARCHAR(255),
+	telefono INT(11),
+	PRIMARY KEY(rut)
+);
+
+CREATE TABLE MATRICULA(
+	codigoMatricula INT(11) AUTO_INCREMENT,
+	rutAlumno INT(11),
+	curso INT(11),
+	nombreApoderado VARCHAR(255),
+	fechaMatricula DATETIME,
+	PRIMARY KEY(codigoMatricula),
+	FOREIGN KEY(rutAlumno) REFERENCES ALUMNO(rut)
+);
